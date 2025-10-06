eaba93a125d36b62221fa729b12cd83dba43f281
softechlimitedappdownloader <softechlimitedappdownloader@gmail.com>
2025-10-06 14:25:30 +0600

    added apk

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fc26ba642a8d3cf494218d9c42eb9cef
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "f9daee1e962e2577a0e19df85882dbc9",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "450f4b66c6e97216da8585b6995880b2",
+"flutter_bootstrap.js": "17638207030d7abf8b9c942d5f32df22",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
